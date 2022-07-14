4346129f7a1c594902a23b57745a892373bf7386
Greeshma <greeshmatm.675@gmail.com>
2022-07-14 08:09:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navfooter from './components/Navfooter';
+import NavHeader from './components/NavHeader';
 
 function App() {
   return (
     <div className="App">
-     
+          <NavHeader/>
+          <Navfooter/>
     </div>
   );
 }
